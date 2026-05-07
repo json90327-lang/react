faf82fdca10e9ea0cb27f04674d4e84374ff0a31
Uday <json90327@gmail.com.id>
2026-05-07 11:47:31 +0530

    "uday"

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-import React from 'react'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import Navbar from './Navbar/Navbar';
-import { Route, BrowserRouter as  Router, Routes } from 'react-router-dom';
-import Home from './Componets/Home';
-import About from './Componets/About';
-import Services from './Componets/Services';
-import Contact from './Componets/Contact';
-
+import Navbar from './Navbar1/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
   return (
     <div>
-         <Router>
-  <Navbar/>
-  <Routes>
-     <Route path='/index' element={<Home/>}/>
-    <Route path='/about' element={<About/>}/>
-     <Route path='/services' element={<Services/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-      
-  </Routes>
-       </Router>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/index' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        
+      </Routes>
+      </Router>
+  
         </div>
   )
 }
---
+++ src/Navbar1/Navbar.jsx
@@ -0,0 +1,54 @@
+// import React from 'react'
+// import { Route } from 'react-router-dom'
+
+// const Navbar = () => {
+//   return    <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
+//                 <a href='/index' className='navbar-brand'>Logo</a>
+//                 <div className='ms-auto'>
+//                     <ul className='navbar-nav'>
+//                         <li><a href="/index" className='nav-link'>Home</a></li>
+//                         <li><a href="/about" className='nav-link'>About</a></li>
+//                         <li><a href="/services" className='nav-link'>Services</a></li>
+//                         <li><a href="/contact" className='nav-link'>Contact</a></li>
+//                     </ul>
+//                 </div>
+//                        </nav> 
+// }
+// export default Navbar
+// import React from 'react'
+// import {Link} from 'react-router-dom';
+// const Navbar = () => {
+//   return   ( 
+//   <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
+//                 <Link to='/index' className='navbar-brand'>Logo</Link>
+//                 <div className='ms-auto'>
+//                     <ul className='navbar-nav'>
+//                         <li><Link to="/index" className='nav-link'>Home</Link></li>
+//                         <li><Link to="/about" className='nav-link'>About</Link></li>
+//                         <li><Link to="/services" className='nav-link'>Services</Link></li>
+//                         <li><Link to="/contact" className='nav-link'>Contact</Link></li>
+//                     </ul>
+//                 </div>
+//             </nav>
+//             )
+  
+// }
+// export default Navbar
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <nav>
+        <link to='/index'>LOGO</link>
+        <div className='ms-auto'>
+            <ul className='navbar-nav'>
+                <li><link to='/index'>Home</link></li>
+                <li><link to='/contact'>Contact</link></li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar;
+
+
